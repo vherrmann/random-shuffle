5a54f3ab78ebffb58215518cc19d14cbe74e6240
Manlio Perillo <manlio.perillo@gmail.com>
2009-03-23 21:52:10 +0100

    Improve error handling
    The internal extractTree function deliberately contained an incomplete
    patterns set.  This was done under the assumption that the missing case
    must not occur in correct code.
    
    Add the missing pattern in the extractTree function, and call the
    ``error`` function, in case of invalid input.

--- src/System/Random/Shuffle.hs
+++ src/System/Random/Shuffle.hs
@@ -45,13 +45,11 @@ buildTree = (fix growLevel) . (map Leaf)
 -- paired with a tree with the element deleted.
 -- The function maintains the invariant of the completeness of the
 -- tree: all internal nodes are always full.
--- NOTE: the collection of patterns below is deliberately not
---       complete.
---       All the missing cases may not occur
---       (and if they do, that's an error).
 extractTree :: Int -> Tree a -> (a, Tree a)
+extractTree _ (Leaf _) = error "invalid element or tree exhausted"
 extractTree 0 (Node _ (Leaf e) r) = (e, r)
 extractTree 1 (Node 2 (Leaf l) (Leaf r)) = (r, Leaf l)
+
 extractTree n (Node c (Leaf l) r) =
     let (e, r') = extractTree (n - 1) r
     in (e, Node (c - 1) (Leaf l) r')
@@ -65,7 +63,6 @@ extractTree n (Node c l@(Node cl _ _) r)
     | otherwise = let (e, r') = extractTree (n - cl) r
                   in (e, Node (c - 1) l r')
 
-
 -- Given a sequence (e1,...en) to shuffle, and a sequence
 -- (r1,...r[n-1]) of numbers such that r[i] is an independent sample
 -- from a uniform random distribution [0..n-i], compute the
@@ -74,6 +71,7 @@ shuffle :: [a] -> [Int] -> [a]
 shuffle elements = shuffleTree (buildTree elements)
     where
       shuffleTree (Leaf e) [] = [e]
+      shuffleTree _ [] = error "sequence exhausted"
       shuffleTree tree (r : rs) =
           let (b, rest) = extractTree r tree
           in b : (shuffleTree rest rs)
